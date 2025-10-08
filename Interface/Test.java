ba660ba8d566b0459913283a682f9fc6499b9b16
Rish2003 <rishabhraj6421@gmail.com>
2025-10-08 13:45:02 +0530

    Multiple Inheritence and Interfaces

---
+++ Interface/Test.java
@@ -0,0 +1,10 @@
+package Interface;
+// import Interface.Dog;
+
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+    }
+}
