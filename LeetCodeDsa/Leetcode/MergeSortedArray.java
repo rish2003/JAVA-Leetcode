c3b99d7f725905a3fd512ef4038d0234eb29c0ba
Rish2003 <rishabhraj6421@gmail.com>
2025-10-15 23:26:15 +0530

    LC-88

---
+++ LeetCodeDsa/Leetcode/MergeSortedArray.java
@@ -0,0 +1,28 @@
+package LeetCodeDsa.Leetcode;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        int j = m - 1;
+        int k = n - 1;
+
+        while(j >= 0 && k >= 0){
+            if(nums1[j] > nums2[k]){
+                nums1[i] = nums1[j];
+                j--;
+                i--;
+            }else{
+                nums1[i] = nums2[k];
+                k--;
+                i--;
+            }
+
+        }
+        while(k >= 0){
+            nums1[i] = nums2[k];
+            i--;
+            k--;
+        }
+
+    }
+}
