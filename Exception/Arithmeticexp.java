c8193cfedda1355a9810bf2d21896e71723f6140
Rish2003 <rishabhraj6421@gmail.com>
2025-10-16 10:15:21 +0530

    Try and Catch

---
+++ Exception/Arithmeticexp.java
@@ -0,0 +1,23 @@
+package Exception;
+
+public class Arithmeticexp {
+    public static void main(String[] args) {
+        int[] num = {100, 200, 300, 400};
+        int[] denom = {2, 2, 0, 2};
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(divide(num[i], denom[i]));
+        }
+
+        System.out.println("Good Job");
+    }
+
+    public static int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            return -1;
+        }
+    }
+}
