64d80b29655f801a62a6af0f1755fc2b55178983
Rish2003 <rishabhraj6421@gmail.com>
2025-10-17 23:52:38 +0530

    String Builder and LC83

---
+++ LeetCodeDsa/Strings/StringBuild.java
@@ -0,0 +1,15 @@
+package LeetCodeDsa.Strings;
+import java.util.ArrayList;
+
+public class StringBuild {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Rishabh");
+        System.out.println(sb);
+        sb.append("Raj");
+        System.out.println(sb);
+        sb.append(10);
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
